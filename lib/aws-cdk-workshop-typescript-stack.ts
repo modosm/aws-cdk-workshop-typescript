ce154013b5c73c48f1a58307617c603269858921
Máté Módos <mate.modos@dakai.io>
2022-11-12 14:39:08 +0100

    Create aws-cdk-workshop-typescript-stack.ts

---
+++ lib/aws-cdk-workshop-typescript-stack.ts
@@ -0,0 +1,19 @@
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export class AwsCdkWorkshopTypescriptStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const lamda =  new lambda.Function(this, "Hello_handler" ,{
+                runtime: lambda.Runtime.NODEJS_16_X,              // execution environment
+                code: lambda.Code.fromAsset('lambda'),      // code loaded from "lambda" directory
+                handler: 'hello.handler'                          // file is "hello", function is "handler"
+            }
+        )
+    }
+}
